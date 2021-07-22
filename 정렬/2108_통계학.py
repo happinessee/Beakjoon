fd1c43312660d52dd88c44e845c74fd2ca036856
gygur106 <gygur1206@gmail.com>
2021-07-22 19:18:13 +0900

    2108_통계학

--- 정렬/2108_통계학.py
+++ 정렬/2108_통계학.py
@@ -12,9 +12,31 @@ print(avg)
 print(arr[n//2])
 
 # 3. 최빈값
-count = []
-for i in range(n) :
-    
+count = 0
+count_cont = 0
+arr_temp = []
+for i in range(len(arr)) :
+    if (count_cont >= 1) :
+        count_cont -= 1
+        continue
+
+    if (count == arr.count(arr[i])) :
+        arr_temp.append(arr[i])
+        count_cont = count - 1
+
+    if (count < arr.count(arr[i])) :
+        count = arr.count(arr[i])
+        count_cont = count - 1
+        arr_temp.clear()
+        arr_temp.append(arr[i])
+
+
+if (len(arr_temp) >= 2) :
+    arr_temp.sort()
+    print(arr_temp[1])
+
+else :
+    print(arr_temp[0])
 
 # 4. 범위
 print(arr[n-1] - arr[0])
