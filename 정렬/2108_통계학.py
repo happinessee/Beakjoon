fa59d9c3619e9ef646c2051d068f2a69968d55b7
gygur106 <gygur1206@gmail.com>
2021-07-23 14:13:51 +0900

    2108_통계학_해결

--- 정렬/2108_통계학.py
+++ 정렬/2108_통계학.py
@@ -1,7 +1,9 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 arr = []
 for i in range(n) :
-    arr.append(int(input()))
+    arr.append(int(sys.stdin.readline()))
 
 arr.sort()
 # 1. 산술평균
@@ -11,32 +13,13 @@ print(avg)
 # 2. 중앙값
 print(arr[n//2])
 
+from collections import Counter
 # 3. 최빈값
-count = 0
-count_cont = 0
-arr_temp = []
-for i in range(len(arr)) :
-    if (count_cont >= 1) :
-        count_cont -= 1
-        continue
-
-    if (count == arr.count(arr[i])) :
-        arr_temp.append(arr[i])
-        count_cont = count - 1
-
-    if (count < arr.count(arr[i])) :
-        count = arr.count(arr[i])
-        count_cont = count - 1
-        arr_temp.clear()
-        arr_temp.append(arr[i])
-
-
-if (len(arr_temp) >= 2) :
-    arr_temp.sort()
-    print(arr_temp[1])
-
+if(n==1) :
+    print(arr[0])
 else :
-    print(arr_temp[0])
+    cnt = Counter(arr).most_common(2)
+    print(cnt[0][0] if cnt[0][1] != cnt[1][1] else cnt[1][0])
 
 # 4. 범위
 print(arr[n-1] - arr[0])
