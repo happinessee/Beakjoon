1b0b2d6ac969414d1ec5d6e30a9f02fe7ffd2f34
gygur106 <gygur1206@gmail.com>
2021-08-02 14:01:03 +0900

    15649 N과 M(1)

--- 백트래킹/15649_N과_M_1.py
+++ 백트래킹/15649_N과_M_1.py
@@ -1,11 +1,31 @@
-import sys
-n, m = map(int, (sys.stdin.readline().split()))
+m, n = map(int, (input().split()))
 
-sequence = []
+arr = []
+for i in range(1, m+1) :
+    arr.append(i)
 
-for i in range(1, m + 1) :
-    sequence.append(i)
 
-for i in range(n) :
-    if (sequence[m - 1].append(sequence[m-1] + 1) :
-        
+v = []
+select = [False] * m
+
+def printFunc() :
+    for i in range(len(v)) :
+        print(v[i], end = " ")
+    print()
+
+def dfs(cnt) :
+    if (cnt == n) :
+        printFunc()
+        return
+
+    for i in range(m) :
+        if(select[i] == True) :
+            continue
+
+        select[i] = True
+        v.append(arr[i])
+        dfs(cnt + 1)
+        v.pop()
+        select[i] = False
+
+dfs(0)
