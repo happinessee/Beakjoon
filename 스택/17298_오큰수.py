310d22b2dbeb3735d61e88277cd3c9f0677510b3
gygur106 <gygur1206@gmail.com>
2021-08-27 13:43:26 +0900

    오큰수 변경중(시간초과)

--- 스택/17298_오큰수.py
+++ 스택/17298_오큰수.py
@@ -2,22 +2,20 @@ import sys
 n = int(sys.stdin.readline())
 
 stack = list(map(int, sys.stdin.readline().split()))
+
+
 answer = []
 
-for i in range(n) :
-    j = i + 1
-    while(True) :
+i = 0
+j = 1
+while(True) :
+    if (stack[i] < stack[j]) :
+        answer.append(stack[j])
+    else :
+        
+        
 
-        if (j == len(stack)) :
-            answer.append(-1)
-            break
-
-        if (stack[i] < stack[j]) :
-            answer.append(stack[j])
-            break
-
-        else :
-            j += 1
+answer.append(-1)
             
 for i in range(n) :
     print(answer[i], end=" ")
